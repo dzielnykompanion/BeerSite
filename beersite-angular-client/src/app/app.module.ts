1920fc07f8c1ebefa78ecd59d8773dd451e50b07
mglbt <golabek.mat@gmail.com>
2020-10-29 10:35:45 +0100

    added by 'ng g c' beer components - create, details, list. by 'ng g s' beer service

--- beersite-angular-client/src/app/app.module.ts
+++ beersite-angular-client/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateBeerComponent } from './create-beer/create-beer.component';
+import { BeerDetailsComponent } from './beer-details/beer-details.component';
+import { BeerListComponent } from './beer-list/beer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateBeerComponent,
+    BeerDetailsComponent,
+    BeerListComponent
   ],
   imports: [
     BrowserModule
