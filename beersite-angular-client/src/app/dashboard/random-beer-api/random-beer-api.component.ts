0510fcedef9ed424f0cac57a3105a4c57ab07f50
mglbt <golabek.mat@gmail.com>
2020-11-22 19:33:42 +0100

    created BrewDogBeer model using json2ts.com

--- beersite-angular-client/src/app/dashboard/random-beer-api/random-beer-api.component.ts
+++ beersite-angular-client/src/app/dashboard/random-beer-api/random-beer-api.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BrewDogBeer } from '../../models/brewDogBeer';
 
 @Component({
   selector: 'app-random-beer-api',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RandomBeerApiComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
   }
+
+  randomBeer: BrewDogBeer;
+
+  private baseUrl = 'https://api.punkapi.com/v2/beers/random';
 
 }
