7c8f1f7f18675e20e6255bf037e5eb8d73de4dbd
mglbt <golabek.mat@gmail.com>
2020-12-29 17:09:03 +0100

    addded materialDatabase to navbar

--- beersite-angular-client/src/app/app-routing.module.ts
+++ beersite-angular-client/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { BeerListComponent } from './beer-list/beer-list.component'
 import { DashboardComponent } from './dashboard/dashboard.component'
 import { RandomBeerApiComponent } from './random-beer-api/random-beer-api.component';
 import { ContactComponent } from './contact/contact.component';
+import { MaterialDatabaseComponent } from './material-database/material-database.component';
 
 export const routes: Routes = [
-  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  { path: '', redirectTo:'/home', pathMatch: 'full'},
   { path: 'home', component: DashboardComponent },
   { path: 'list', component: BeerListComponent },
   { path: 'randomBeer', component: RandomBeerApiComponent},
-  { path: 'contact', component: ContactComponent}
+  { path: 'contact', component: ContactComponent},
+  { path: 'materialDatabase', component: MaterialDatabaseComponent}
 ];
 
 @NgModule({
